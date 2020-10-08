17cc4503f14756c5f4bbb65aa8f7d2cc9505e02b
Rob Chapman <autosexual@gmail.com>
2020-10-08 22:17:27 +0900

    model validations added

--- app/models/force.rb
+++ app/models/force.rb
@@ -1,4 +1,8 @@
 class Force < ApplicationRecord
   belongs_to :user
   has_many :forces, dependent: :destroy
+
+  FACTIONS = ['Imperium', 'Chaos', 'Aeldari', 'Tyranids', 'Orks', 'Necrons', 'Tau Empire']
+  validates :name, presence: true
+  validates :faction, presence: true, inclusion: { in: FACTIONS }
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,3 +1,7 @@
 class Unit < ApplicationRecord
   belongs_to :force
+
+  ROLES = ['HQ', 'Elite', 'Troops', 'Fast Attack', 'Heavy Support', 'Lord of War', 'Other']
+  validates :name, presence: true
+  validates :role, presence: true, inclusion: { in: ROLES }
 end
