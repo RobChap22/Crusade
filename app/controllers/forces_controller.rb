9cbc0a46e0349170968073d8bad94fd00d9d207e
Rob Chapman <autosexual@gmail.com>
2020-10-09 21:38:05 +0900

    new form working

--- app/controllers/forces_controller.rb
+++ app/controllers/forces_controller.rb
@@ -7,4 +7,18 @@ class ForcesController < ApplicationController
   def show
     @force = Force.find(params[:id])
   end
+
+  def new
+    @force = Force.new
+  end
+
+  def create
+    @force = Force.new(force_params)
+    @force.user = current_user
+    if @force.save
+      redirect_to force_path(@force)
+    else
+      render :new
+    end
+  end
 end
