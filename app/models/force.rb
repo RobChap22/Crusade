d10595af756a4536060e3497fbcddd3395540307
Rob Chapman <autosexual@gmail.com>
2020-10-10 18:07:00 +0900

    defaults for force model operational and switchable

--- app/models/force.rb
+++ app/models/force.rb
@@ -1,5 +1,6 @@
 class Force < ApplicationRecord
-  # before_create :set_supply
+  before_create :set_default_values
+
   belongs_to :user
   has_many :units, dependent: :destroy
 
@@ -9,8 +10,12 @@ class Force < ApplicationRecord
 
   private
 
-  # def set_supply
-  #   self.
-  # end
+  def set_default_values
+    self.power ? self.supply_limit = 50 : self.supply_limit = 1000
+    self.battle_tally = 0
+    self.battles_won = 0
+    self.requisition_points = 5
+    self.supply_used = 0
+  end
 
 end
